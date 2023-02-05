0ab1e6d5548dde42a8e16343c3038ea42998404c
devblixt <devblixt@gmail.com>
2023-02-06 00:23:05 +0530

    run1 and run2 have been manifested

--- Assignment 5/run2.sh
+++ Assignment 5/run2.sh
@@ -1,5 +1,4 @@
 cp schedule.c /usr/src/minix/servers/sched/;
-cp system.c /usr/src/minix/kernel/;
-cp -R /workloads_lab5 ../home/byte-unixbench-mod/UnixBench/
+cp -R /workloads_lab5 ../home/byte-unixbench-mod/UnixBench/workload_mix/;
 cd /usr/src/;
 make build MKUPDATE=yes >log.txt 2>log.txt
