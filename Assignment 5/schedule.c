ddde48af16ad316a695e6e8edb1bd2c3d6447d73
devblixt <devblixt@gmail.com>
2023-02-07 15:42:15 +0530

    Change comments final

--- Assignment 5/schedule.c
+++ Assignment 5/schedule.c
@@ -101,7 +101,7 @@ int do_noquantum(message *m_ptr)
 
 	rmp = &schedproc[proc_nr_n];
 	if (rmp->priority < MIN_USER_Q) {
-		rmp->priority -= 1; // lower priority
+		rmp->priority -= 1; // higher priority
 	}
 
 	if ((rv = schedule_process_local(rmp)) != OK) {
