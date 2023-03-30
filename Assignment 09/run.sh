7345dcdb3a8be6b916de572a14d9b77fff8d4f71
Siddharth P <90827725+Whitelisted2@users.noreply.github.com>
2023-03-30 20:03:04 +0530

    we do a lil bit of restructuring

---
+++ Assignment 09/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cp open.c /usr/src/minix/servers/vfs/open.c;
+cp read.c /usr/src/minix/servers/vfs/read.c;
+cp link.c /usr/src/minix/servers/vfs/link.c;
+
+cd /usr/src;
+
+make build MKUPDATE=yes > log.txt;
